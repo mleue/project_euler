426bdf4320809bc4794d5833cc0bcfc1176913e0
nilmagnum <leue@student.chalmers.se>
2016-03-16 10:14:46 +0100

    cpp solution to problem 0012

---
+++ problem_0012/cpp/0012.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+//TODO: this method takes like 10mins to run ;)
+//TODO: think rather than brute force
+long triangleNumberMoreThanLimitDivisors(int limit) {
+	long triangleNumber = 0;
+	int i = 1;
+	while (true) {
+		triangleNumber += i;
+		int divisors = 0;
+		for (int j=1; j<triangleNumber+1; ++j) {
+			if (triangleNumber%j == 0)
+				++divisors;
+			if (j > triangleNumber/20 && divisors < limit/10)
+				break;
+		}
+		if (divisors > limit)
+			return triangleNumber;
+		++i;
+	}
+}
+
+int main() {
+	int limit = 500;
+	long triangleNumber = triangleNumberMoreThanLimitDivisors(limit);
+
+	std::cout << "The first triangle number with more than " << limit << " divisors is: " << triangleNumber << std::endl;
+}
