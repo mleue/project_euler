85b9a5d870615105a519b2fda2e55c4582931ec3
nilmagnum <leue@student.chalmers.se>
2016-04-10 15:06:52 +0200

    ruby solution to problem 0032

--- problem_0032/ruby/0032.rb
+++ problem_0032/ruby/0032.rb
@@ -1,7 +1,25 @@
-arr = [1,2,3,4,5,6,7,8,9]
+def pandigital_products()
+	arr = [1,2,3,4,5,6,7,8,9]
+	products = []
 
-p arr.permutation(4).to_a.length
-p arr.permutation(3).to_a.length
-arr.permutation(2).to_a.each do |n|
-	puts n.join.to_i
+	(1..9999).each do |i|
+		(1..9999).each do |j|
+			product = i*j
+			if product > 9999
+				next
+			end
+			arr_compare = i.to_s.chars.map(&:to_i) + j.to_s.chars.map(&:to_i) + product.to_s.chars.map(&:to_i)
+			if (arr - arr_compare).empty?
+				puts "#{i}x#{j} = #{product}"
+				products.push(product)
+			end
+		end
+	end
+
+	return products
 end
+
+start = Time.now
+products = pandigital_products()
+puts "The sum of all 1-9 pandigital products is: #{products.uniq.inject(:+)}"
+puts Time.now-start
