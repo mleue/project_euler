296efe944737e481b2d8d653471fd2943176be4d
nilmagnum <leue@student.chalmers.se>
2016-04-10 15:29:54 +0200

    slightly faster by using a lookup table for the factorials now

--- problem_0034/ruby/0034.rb
+++ problem_0034/ruby/0034.rb
@@ -4,14 +4,19 @@ class Integer
 	end
 end
 
-def digit_factorials
+factorials = (0..9).map(&:fact)
+
+def digit_factorials(factorials)
 	(3...3000000).each do |n|
-		if n.to_s.chars.map(&:to_i).map(&:fact).inject(:+) == n
+		if n.to_s.chars.map(&:to_i).map{|n| factorials[n]}.inject(:+) == n
 			puts n
 		end
 	end
 end
 
 start = Time.now
-digit_factorials()
+digit_factorials(factorials)
 puts Time.now - start
+
+#for every digit, there is a max of fact(9) ~ 350k added to the final sum
+#therefore we only have to check the numbers until about 3mio
