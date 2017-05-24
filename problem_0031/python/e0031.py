6a58f51c040811063523b1e90ecca0e9069fc6cf
mleue <domus@posteo.de>
2017-05-24 12:22:15 +0200

    add recursive python solution to problem 0031

--- problem_0031/python/e0031.py
+++ problem_0031/python/e0031.py
@@ -1,61 +1,16 @@
-import math
+coins = (200, 100, 50, 20, 10, 5, 2, 1)
 
-target = 2.0
-values = (2.0, 1.0, 0.5, 0.2, 0.1, 0.05, 0.02)
-all_values = [[i*val for i in range(0, int(target/val) + 1)] for val in values]
-two_pound_values, one_pound_values, fifty_p_values, twenty_p_values, ten_p_values, five_p_values, two_p_values = all_values
-
-count = 0
-for two_pounds in two_pound_values:
-  curr_sum = two_pounds
-  if curr_sum == target:
-    count += 1
-    break;
-  elif curr_sum > target:
-    break
-  for one_pound in one_pound_values:
-    curr_sum = two_pounds + one_pound
-    if curr_sum == target:
+def possibilities(target, maxcoin):
+  count = 0
+  #if we can fill up with 1p coins, we can always add 1
+  if maxcoin == 7:
+    return 1
+  for i in range(maxcoin, 8):
+    rest = target - coins[i]
+    if rest == 0:
       count += 1
-      break;
-    elif curr_sum > target:
-      break
-    for fifty_p in fifty_p_values:
-      curr_sum = two_pounds + one_pound + fifty_p
-      if curr_sum == target:
-        count += 1
-        break;
-      elif curr_sum > target:
-        break
-      for twenty_p in twenty_p_values:
-        curr_sum = two_pounds + one_pound + fifty_p + twenty_p
-        if curr_sum == target:
-          count += 1
-          break;
-        elif curr_sum > target:
-          break
-        for ten_p in ten_p_values:
-          curr_sum = two_pounds + one_pound + fifty_p + twenty_p + ten_p
-          if curr_sum == target:
-            count += 1
-            break;
-          elif curr_sum > target:
-            break
-          for five_p in five_p_values:
-            curr_sum = two_pounds + one_pound + fifty_p + twenty_p + ten_p + five_p
-            if curr_sum == target:
-              count += 1
-              break;
-            elif curr_sum > target:
-              break
-            for two_p in two_p_values:
-              curr_sum = two_pounds + one_pound + fifty_p + twenty_p + ten_p + five_p + two_p
-              if curr_sum == target:
-                count += 1
-                break;
-              elif curr_sum > target:
-                break
-              else:
-                count += 1
+    elif rest > 0:
+      count += possibilities(rest, i)
+  return count
 
-print(count)
+print(possibilities(200, 0))
